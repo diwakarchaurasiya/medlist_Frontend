1df968182aa102c24b603f7d0eb7d0042dd9606e
diwakar chaurasiya <diwakarbmj1212@gmail.com>
2024-12-29 11:50:01 +0530

    Added fronten

--- src/components/Signup/Login.jsx
+++ src/components/Signup/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -106,9 +107,9 @@ const Login = () => {
           </button>
         </form>
         <p className="text-center text-sm mt-4">
-          <a href="#" className="text-primary hover:underline">
-            Forgot Password?
-          </a>
+          Don't Have Account?<Link to={`/${selectedUser}/register`}  className="text-primary hover:underline">
+           Register Here
+          </Link>
         </p>
       </div>
     </div>
